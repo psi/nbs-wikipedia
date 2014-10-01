d865338edb789d78e0bd7241016f11f98efc1f6f
JD Harrington <jd@jdharrington.net>
2014-10-01 06:51:31 -0400

    go fmt

--- importer.go
+++ importer.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-  "bufio"
-  //"fmt"
-  "log"
-  "os"
-  "regexp"
-  "strconv"
-  "strings"
-  "gopkg.in/mgo.v2"
-  //"gopkg.in/mgo.v2/bson"
+	"bufio"
+	//"fmt"
+	"gopkg.in/mgo.v2"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	//"gopkg.in/mgo.v2/bson"
 )
 
 type Page struct {
-  Language string
-  Title    string
-  Views    int
+	Language string
+	Title    string
+	Views    int
 }
 
 func main() {
-  session, err := mgo.Dial("localhost")
-  if err != nil {
-    panic(err)
-  }
-  defer session.Close()
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 
-  pages := session.DB("wikipedia").C("pages")
+	pages := session.DB("wikipedia").C("pages")
 
-  file, err := os.Open("/tmp/pagecounts.csv")
-  if err != nil {
-    panic(err)
-  }
+	file, err := os.Open("/tmp/pagecounts.csv")
+	if err != nil {
+		panic(err)
+	}
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    fields := strings.Split(scanner.Text(), " ")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), " ")
 
-    language := fields[0]
-    title    := fields[1]
+		language := fields[0]
+		title := fields[1]
 
-    // We only want pages from Wikipedia, not other projects
-    isNotWikipediaPage, err := regexp.MatchString("\\.", language)
-    if err!= nil {
-      panic(err)
-    }
+		// We only want pages from Wikipedia, not other projects
+		isNotWikipediaPage, err := regexp.MatchString("\\.", language)
+		if err != nil {
+			panic(err)
+		}
 
-    // Skip titles that contain prefixes like "Special:", "User:", etc.
-    isNonStandardPage, err := regexp.MatchString(":", title)
-    if err != nil {
-      panic(err)
-    }
+		// Skip titles that contain prefixes like "Special:", "User:", etc.
+		isNonStandardPage, err := regexp.MatchString(":", title)
+		if err != nil {
+			panic(err)
+		}
 
-    if !isNotWikipediaPage && !isNonStandardPage {
-      views, err := strconv.Atoi(fields[3])
-      if err != nil {
-        panic(err)
-      }
+		if !isNotWikipediaPage && !isNonStandardPage {
+			views, err := strconv.Atoi(fields[3])
+			if err != nil {
+				panic(err)
+			}
 
-      err = pages.Insert(&Page{language, title, views})
+			err = pages.Insert(&Page{language, title, views})
 
-      if err != nil {
-        log.Fatal(err)
-      }
-    }
-  }
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 }
--- reporter.go
+++ reporter.go
@@ -1,48 +1,50 @@
 package main
 
 import (
-  //"bufio"
-  "fmt"
-  //"log"
-  //"os"
-  //"regexp"
-  //"strconv"
-  //"strings"
-  "gopkg.in/mgo.v2"
-  "gopkg.in/mgo.v2/bson"
+	//"bufio"
+	"fmt"
+	//"log"
+	//"os"
+	//"regexp"
+	//"strconv"
+	//"strings"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Page struct {
-  Language string
-  Title    string
-  Views    int
+	Language string
+	Title    string
+	Views    int
 }
 
 func main() {
-  session, err := mgo.Dial("localhost")
-  if err != nil {
-    panic(err)
-  }
-  defer session.Close()
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
 
-  pages := session.DB("wikipedia").C("pages")
+	pages := session.DB("wikipedia").C("pages")
 
-  var languages []string
-  err = pages.Find(bson.M{}).Distinct("language", &languages)
+	var languages []string
+	err = pages.Find(bson.M{}).Distinct("language", &languages)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  for i := 0; i < len(languages); i++ {
-    var topPages []Page
-    pages.Find(bson.M{ "language": languages[i] }).Sort("-views").Limit(5).All(&topPages)
+	for i := 0; i < len(languages); i++ {
+		var topPages []Page
+		pages.Find(bson.M{"language": languages[i]}).Sort("-views").Limit(10).All(&topPages)
 
-    fmt.Println(languages[i], ":")
-    for j := 0; j < len(topPages); j++ {
-      fmt.Println(topPages[j].Title, ",", topPages[j].Views)
-    }
-  }
+		fmt.Println(languages[i], ":")
+		for j := 0; j < len(topPages); j++ {
+			fmt.Println(topPages[j].Title, ",", topPages[j].Views)
+		}
 
-  fmt.Println("Found ", len(languages), " languages")
+		fmt.Println()
+	}
+
+	fmt.Println("Found ", len(languages), " languages")
 }
